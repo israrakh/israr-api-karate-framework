68c402a2b85e607e815979ea930d0714bbc61e9b
Israr <israr.akh03@gmail.com>
2023-05-22 21:50:49 -0400

    Modified TestRunner Class

--- src/test/java/api/runners/TestRunner.java
+++ src/test/java/api/runners/TestRunner.java
@@ -5,9 +5,10 @@ import com.intuit.karate.junit5.Karate;
 public class TestRunner {
 
 	// run() method required path to feature files.
-	// you can use tags() method to specify the Tags.
+	// can use tags() method to specify the Tags.
 	@Karate.Test
 	public Karate runTests() {
 		return Karate.run("classpath:features").tags("Regression");
 	}
+
 }
